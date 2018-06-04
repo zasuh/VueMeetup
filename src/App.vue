717800ef1ce0e23928a38b574fce902a6b8bc268
Zane Suhadolnik <zanesuhi@gmail.com>
2018-06-04 07:48:46 +0200

    Initial commit, added navigation bar, collapsable burger menu

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="green">
+      <v-toolbar-side-icon 
+      @click.native.stop="sideNav = !sideNav"
+      class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false
+    }
+  },
+  name: 'App'
+}
+</script>
